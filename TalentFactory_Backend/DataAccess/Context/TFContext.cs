3b8a4c0ee1e3da96be78550e2540a7e543d56682
Jeroen <valcke_jeroen@hotmail.com>
2014-05-31 16:13:44 +0200

    conf + klasses

---
+++ TalentFactory_Backend/DataAccess/Context/TFContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TalentFactory_CMS.Models;
+
+namespace TalenFactory_Backend.DataAccess.Context
+{
+    public class TFContext : DbContext
+    {
+        public DbSet<Award> Award { get; set; }
+        public DbSet<Contact> Contact { get; set; }
+        
+    }
+}
---
+++ TalentFactory_Backend/DataAccess/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TalenFactory_Backend.DataAccess.Repositories
+{
+
+    public interface IGenericRepository<TEntity>
+      where TEntity : class {
+        IEnumerable<TEntity> All();
+        void Delete(object id);
+        void Delete(TEntity entityToDelete);
+        TEntity GetByID(object id);
+        TEntity Insert(TEntity entity);
+        void Update(TEntity entityToUpdate);
+
+    }
+
+}
