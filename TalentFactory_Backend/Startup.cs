96301520ecdcb4322c832030e5c8f2daeb7eca06
Jeroen <valcke_jeroen@hotmail.com>
2014-05-31 15:58:42 +0200

    init

---
+++ TalentFactory_Backend/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TalentFactory_Backend.Startup))]
+
+namespace TalentFactory_Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
