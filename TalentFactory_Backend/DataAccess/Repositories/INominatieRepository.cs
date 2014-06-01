cdaa7af2b7d16b79e4f1ce58bf53ce9823afe2ec
Jeroen <valcke_jeroen@hotmail.com>
2014-06-01 15:24:08 +0200

    entertje

--- TalentFactory_Backend/DataAccess/Repositories/INominatieRepository.cs
+++ TalentFactory_Backend/DataAccess/Repositories/INominatieRepository.cs
@@ -8,6 +8,7 @@ namespace TalentFactory_Backend.DataAccess.Repositories
         System.Collections.Generic.List<TalentFactory_CMS.Models.Nominatie> GetNominatiesByAward(int id);
         System.Collections.Generic.List<TalentFactory_CMS.Models.Nominatie> GetNominatiesByAward(TalentFactory_CMS.Models.Award award);
         List<Nominatie> GetNominatiesByIndex(int awardId, int iStartIndex, int iAantal);
+
         // System.Collections.Generic.List<TalentFactory_CMS.Models.Nominatie> GetNominatiesByIndex(int iStartIndex, int iEindIndex);
     }
 }
